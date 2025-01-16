1c9dfc9de7e3d42dc0a8ebc2a97b2c6bd612b0fb
shoravs <sharmasaurav142@gmail.com>
2025-01-16 19:23:11 +0530

    return indices of the two numbers such that they add up to target

--- src/com/leet/arrays/easy/Solution1.java
+++ src/com/leet/arrays/easy/Solution1.java
@@ -5,13 +5,25 @@ Given an array of integers nums and an integer target, return indices of the two
  */
 public class Solution1 {
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int target = 9;
-        System.out.println(twoSum(nums, target));
+        twoSum(nums, target);
     }
 
-    static int[] twoSum(int[] nums, int target) {
+    static void twoSum(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
 
-        return null;
+        while (low < high) {
+            if (nums[low] + nums[high] == target) {
+                System.out.println("[" + low + ", " + high + "]");
+                low++;
+                high--;
+            }
+            if (nums[low] + nums[high] > target)
+                high--;
+            if (nums[low] + nums[high] < target)
+                low++;
+        }
     }
 }
