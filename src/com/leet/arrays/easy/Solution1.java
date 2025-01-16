485b1896634427fe40a2951628c298005332b1b4
shoravs <sharmasaurav142@gmail.com>
2025-01-16 16:44:34 +0530

    add comments

--- src/com/leet/arrays/easy/Solution1.java
+++ src/com/leet/arrays/easy/Solution1.java
@@ -1,7 +1,17 @@
 package com.leet.arrays.easy;
 
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ */
 public class Solution1 {
     public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(twoSum(nums, target));
+    }
 
+    static int[] twoSum(int[] nums, int target) {
+
+        return null;
     }
 }
