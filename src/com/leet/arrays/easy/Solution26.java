46e3872c025a27445590616684d77addc6fd1843
shoravs <sharmasaurav142@gmail.com>
2025-01-19 12:51:33 +0530

    add efficient solution

--- src/com/leet/arrays/easy/Solution26.java
+++ src/com/leet/arrays/easy/Solution26.java
@@ -1,6 +1,5 @@
 package com.leet.arrays.easy;
 
-import java.util.Arrays;
 
 /*
 remove-duplicates-from-sorted-array
@@ -8,8 +7,8 @@ return the number of unique elements in nums.
  */
 public class Solution26 {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2};
-        System.out.println(removeDuplicates(nums));
+        int[] nums = {1, 1, 2};
+        System.out.println(removeDuplicates2(nums));
     }
 
     /*
@@ -19,6 +18,7 @@ public class Solution26 {
         int[] temp = new int[nums.length];
         temp[0] = nums[0];
         int res = 1;
+        int count = 0;
 
         for (int i = 1; i < nums.length; i++)
             if (temp[res - 1] != nums[i]) {
@@ -26,8 +26,21 @@ public class Solution26 {
                 res++;
             }
 
-        System.out.println(Arrays.toString(temp));
+        return res;
+    }
 
-        return temp.length;
+    /*
+    Efficient solution
+     */
+    static int removeDuplicates2(int[] nums) {
+        int res = 1;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[res - 1] != nums[i]) {
+                nums[res] = nums[i];
+                res++;
+            }
+
+        return res;
     }
 }
