eee43433e1483a1185c2daf821bebb96ece28bdd
shoravs <sharmasaurav142@gmail.com>
2025-01-20 22:32:01 +0530

    add solution for 27

---
+++ src/com/leet/arrays/easy/Solution27.java
@@ -0,0 +1,18 @@
+package com.leet.arrays.easy;
+
+/*
+remove all occurrences of val in nums in-place. The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+ */
+public class Solution27 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(removeElement(nums, 3));
+    }
+
+    static int removeElement(int[] nums, int val) {
+
+        return 0;
+    }
+
+}
